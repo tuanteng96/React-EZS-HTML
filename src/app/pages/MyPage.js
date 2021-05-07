fcb1fadaccfc31a7df20ab651188c2353644e613
tuanteng96 <tuantengyn@gmail.com>
2021-05-07 22:22:37 +0700

    update

---
+++ src/app/pages/MyPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {useSubheader} from "../../_metronic/layout";
+import "./../../_metronic/_assets/sass/pages/my-page/my-page.scss";
+
+export const MyPage = () => {
+  const suhbeader = useSubheader();
+  suhbeader.setTitle("My Custom title");
+
+  return (<><h1 className="my-page">My Page</h1></>);
+};
